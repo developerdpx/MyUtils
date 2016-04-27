1b3793f912588b52a134369069ff3a58c1248092
daipiaoxiang <daipiaoxiang@pcw268.com>
2016-04-27 18:28:52 +0800

    提交

--- app/src/main/java/com/v/myutils/MainActivity.java
+++ app/src/main/java/com/v/myutils/MainActivity.java
@@ -3,10 +3,18 @@ package com.v.myutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.view.OnChangedListener;
+import com.view.SlipButton;
 
 import org.w3c.dom.Document;
 import org.xutils.DbManager;
@@ -19,6 +27,7 @@ import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity {
@@ -27,15 +36,48 @@ public class MainActivity extends BaseActivity {
     private Button mybuo;
     @ViewInject(R.id.img)
     private ImageView img;
+    @ViewInject(R.id.gridview)
+    private GridView gridview;
+    private List<String> list;
+    private SlipButton sb;
+    private SlipButton sb1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         x.view().inject(this);
+
+        sb= (SlipButton) findViewById(R.id.sb);
+        sb.setNowChoose(true);
+        sb1= (SlipButton) findViewById(R.id.sb1);
+        sb.setOnChangeListener(new OnChangedListener() {
+            @Override
+            public void OnChanged(boolean CheckState) {
+                String s;
+                if(sb1.isNowChoose())
+                {
+                    s="Button2打开了...";
+                }else{
+                    s="Button2关闭了...";
+                }
+                if (CheckState)
+                    Toast.makeText(MainActivity.this, "Button1打开了..."+s,
+                            Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Button1关闭了..."+s,
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
         //从数据库中获取数据
         getDbData();
+        list = new ArrayList<String>();
+        for (int i = 0; i < 21; i++) {
+            list.add("第几个的=" + i);
+        }
 
+        Myadpter myadpter = new Myadpter(list);
+        gridview.setAdapter(myadpter);
         RequestParams params = new RequestParams("http://blog.csdn.net/mobile/experts.html");
         x.http().get(params, new Callback.CacheCallback<String>() {
             @Override
@@ -88,6 +130,7 @@ public class MainActivity extends BaseActivity {
         } catch (DbException e) {
             e.printStackTrace();
         }
+
     }
 
 
@@ -112,6 +155,49 @@ public class MainActivity extends BaseActivity {
                 .setFailureDrawableId(R.mipmap.ic_launcher)//加载失败后默认显示图片
                 .build();
         x.image().bind(img, "http://pic.baike.soso.com/p/20090711/20090711101754-314944703.jpg", imageOptions);
+    }
+
+    class Myadpter extends BaseAdapter {
+        List<String> list;
+
+        public Myadpter(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Hadel hadel;
+            if (convertView == null) {
+                hadel = new Hadel();
+                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_gridview, null);
+                hadel.txt = (TextView) convertView.findViewById(R.id.txt);
+                convertView.setTag(hadel);
+            } else {
+                hadel = (Hadel) convertView.getTag();
+            }
+            hadel.txt.setText(list.get(position).toString());
+            return convertView;
+        }
+
+        class Hadel {
+            TextView txt;
+        }
+
     }
 
 
